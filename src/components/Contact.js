c5fd1a1c7a8422a398017bd3ceb77af4c5b3ae79
Romeo-ag <romeo.a.gurning@gmail.com>
2022-06-20 09:33:36 -0400

    menambahkan komponen header dan contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
+import "./contact.css"
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({contacts}) => {
     return (
-        <></>
+        <div className='contact'>
+            <div className="detail">
+            <div className='name'><p>{contacts.name}</p></div>
+            <div className='phone'><p>{contacts.phone}</p></div>
+            <div className='email'><p>{contacts.email}</p></div>        
+            </div>
+            <div className='photo'><img src={contacts.photo} alt={contacts.photo}></img></div>
+        </div>
+
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import "./header.css";
+import parse from 'html-react-parser'
+
+const createLine = (times)=>{
+    const dot = '&#x2B29';
+    return dot.repeat(times)
+}
 
 const Header = () => {
     return (
-        <></>
+        
+        <div className='header'>
+            <div className='title'>Call a Friend</div>
+            <div className='subtitle'>Your friendly contact app </div>
+            <div className='line'>{parse(createLine(80))}</div>
+        </div>
+        
     )
 }
 
